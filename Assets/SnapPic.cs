45fd0676cc4aa5123eaac9156a30fcd2f017c7e4
Muthu <muthuvijayakumar5@gmail.com>
2018-10-30 22:54:07 +0530

    My AR project

---
+++ Assets/SnapPic.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class SnapPic : MonoBehaviour
+{
+    private int num = 0;
+    private int picNum = 1;
+    public GameObject glass;
+    public GameObject mask;
+
+    public void TakePic()
+    {
+        picNum += num;
+        ScreenCapture.CaptureScreenshot(Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments),"IMG.png"));
+        Debug.Log("screenShot Taken");
+    }
+    public void choosingSpecs()
+    {
+        glass.SetActive(true);
+        mask.SetActive(false);
+
+    }
+    public void choosingmask()
+    {
+        glass.SetActive(false);
+        mask.SetActive(true);
+
+    }
+
+
+}
